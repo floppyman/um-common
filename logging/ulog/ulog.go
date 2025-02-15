d611651111c03eac55b165c39ac42f6214f51d28
umbrella <198265696+umbrella-sh@users.noreply.github.com>
2025-02-15 12:09:49 +0100

    updated ulog to support configuration with config struct and setting time format

--- logging/ulog/ulog.go
+++ logging/ulog/ulog.go
@@ -6,8 +6,19 @@ import (
 
 	"github.com/phuslu/log"
 	"github.com/robfig/cron/v3"
+
 	"github.com/umbrella-sh/um-common/ext"
 )
+
+type UmbrellaLoggerConfig struct {
+	FileSize       int64  `default:"10"`
+	FileBackups    int    `default:"1"`
+	AllFileName    string `default:"all"`
+	ErrorFileName  string `default:"error"`
+	LogsFolderPath string `default:"./logs"`
+	IncludeCaller  bool   `default:"false"`
+	LogTimeFormat  string `default:"2006.01.02 15:04:05.000"`
+}
 
 type UmbrellaLogger struct {
 	Console log.Logger
@@ -16,6 +27,7 @@ type UmbrellaLogger struct {
 
 var logger UmbrellaLogger
 var isInitialized = false
+var config UmbrellaLoggerConfig
 
 // Console writes the logs directly to the console (not speed of writing is different on different OSs)
 var Console log.Logger
@@ -23,18 +35,44 @@ var Console log.Logger
 // File writes the logs directly to a log file, either the 'all' file and/or the 'error' file.
 var File log.Logger
 
-// New creates an instance of the logger and sets the current instance.
+// New creates an instance of the logger and sets the current instance config to the provided.
+//
+//goland:noinspection GoUnusedExportedFunction
+func New(conf UmbrellaLoggerConfig) {
+	if isInitialized {
+		logger.Console.Trace().Msg("You have already initialized the logger, it cannot be done again.")
+	}
+
+	config = conf
+
+	initLogger()
+
+	Console = logger.Console
+	File = logger.File
+
+	initRotateTask()
+
+	isInitialized = true
+}
+
+// NewSimple creates an instance of the logger and sets the current instance.
 // Taking a 'fileSize' of the log files (all and error).
 // The number `fileBackups` to keep for each log file.
 // And a bool indicating if the caller+line number, should be included.
 //
 //goland:noinspection GoUnusedExportedFunction
-func New(fileSize int64, fileBackups int, includeCaller bool) {
+func NewSimple(fileSize int64, fileBackups int, includeCaller bool) {
 	if isInitialized {
 		logger.Console.Trace().Msg("You have already initialized the logger, it cannot be done again.")
 	}
 
-	initLogger(fileSize, fileBackups, "all", "error", "./logs", includeCaller)
+	config = UmbrellaLoggerConfig{
+		FileSize:      fileSize,
+		FileBackups:   fileBackups,
+		IncludeCaller: includeCaller,
+	}
+
+	initLogger()
 
 	Console = logger.Console
 	File = logger.File
@@ -57,7 +95,16 @@ func NewCustom(fileSize int64, fileBackups int, allFileName string, errorFileNam
 		logger.Console.Trace().Msg("You have already initialized the logger, it cannot be done again.")
 	}
 
-	initLogger(fileSize, fileBackups, allFileName, errorFileName, logsFolderPath, true)
+	config = UmbrellaLoggerConfig{
+		FileSize:       fileSize,
+		FileBackups:    fileBackups,
+		AllFileName:    allFileName,
+		ErrorFileName:  errorFileName,
+		LogsFolderPath: logsFolderPath,
+		IncludeCaller:  true,
+	}
+
+	initLogger()
 
 	Console = logger.Console
 	File = logger.File
@@ -81,7 +128,16 @@ func NewCustomFull(fileSize int64, fileBackups int, allFileName string, errorFil
 		logger.Console.Trace().Msg("You have already initialized the logger, it cannot be done again.")
 	}
 
-	initLogger(fileSize, fileBackups, allFileName, errorFileName, logsFolderPath, includeCaller)
+	config = UmbrellaLoggerConfig{
+		FileSize:       fileSize,
+		FileBackups:    fileBackups,
+		AllFileName:    allFileName,
+		ErrorFileName:  errorFileName,
+		LogsFolderPath: logsFolderPath,
+		IncludeCaller:  includeCaller,
+	}
+
+	initLogger()
 
 	Console = logger.Console
 	File = logger.File
@@ -91,31 +147,34 @@ func NewCustomFull(fileSize int64, fileBackups int, allFileName string, errorFil
 	isInitialized = true
 }
 
-func initLogger(fileSize int64, fileBackups int, allFileName string, errorFileName string, logsFolderPath string, includeCaller bool) {
+func initLogger() {
 	logger = UmbrellaLogger{
 		Console: log.Logger{
-			TimeFormat: "2006.01.02 15:04:05.000",
-			Caller:     ext.Iif(includeCaller, 1, 0),
+			TimeFormat: config.LogTimeFormat,
+			Caller:     ext.Iif(config.IncludeCaller, 1, 0),
 			Writer: &log.ConsoleWriter{
 				ColorOutput: true,
 			},
 		},
 		File: log.Logger{
-			Level: log.TraceLevel,
+			TimeFormat: config.LogTimeFormat,
+			Level:      log.TraceLevel,
 			Writer: &log.MultiLevelWriter{
 				ConsoleWriter: &log.FileWriter{
-					Filename:     filepath.Join(logsFolderPath, allFileName),
-					MaxSize:      fileSize * 1024 * 1024, // 100mb
-					MaxBackups:   fileBackups,
-					LocalTime:    false,
+					TimeFormat:   config.LogTimeFormat,
+					Filename:     filepath.Join(config.LogsFolderPath, config.AllFileName),
+					MaxSize:      config.FileSize * 1024 * 1024, // 100mb
+					MaxBackups:   config.FileBackups,
+					LocalTime:    true,
 					EnsureFolder: true,
 				},
 				ConsoleLevel: log.TraceLevel,
 				ErrorWriter: &log.FileWriter{
-					Filename:     filepath.Join(logsFolderPath, errorFileName),
-					MaxSize:      fileSize * 1024 * 1024, // 100mb
-					MaxBackups:   fileBackups,
-					LocalTime:    false,
+					TimeFormat:   config.LogTimeFormat,
+					Filename:     filepath.Join(config.LogsFolderPath, config.ErrorFileName),
+					MaxSize:      config.FileSize * 1024 * 1024, // 100mb
+					MaxBackups:   config.FileBackups,
+					LocalTime:    true,
 					EnsureFolder: true,
 				},
 			},
