df523262142b367113a93d9aec42258fea310137
Lasse Nielsen <floppyman@al1as.dk>
2025-06-02 21:01:59 +0200

    added comments to SleepContext and added a return value to use for checking if needed

--- timers/sleep.go
+++ timers/sleep.go
@@ -5,9 +5,13 @@ import (
 	"time"
 )
 
-func SleepContext(ctx context.Context, delay time.Duration) {
+// SleepContext waits for the specified delay, but exits early if the context.Done() is called
+// will return true, if it was cancelled with context, else false
+func SleepContext(ctx context.Context, delay time.Duration) bool {
 	select {
 	case <-ctx.Done():
+		return true
 	case <-time.After(delay):
 	}
+	return false
 }
